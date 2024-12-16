6e4e8eb5e872ed8b65aab01f6cd16049664f3cf5
lordsavor <63599521+lordsavor@users.noreply.github.com>
2024-12-16 19:16:52 +0800

    Pre-running
    additional github code before going deploying

--- travel_journal/src/components/Navbar.js
+++ travel_journal/src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return(
         <div>
             <nav className="navbar--main">
-                <h1><i className='bx bxs-briefcase-alt briefcase'></i> My Travel Plan</h1>
+                <h1><i className='bx bxs-briefcase-alt briefcase'></i> Aristo Sinatra Gunawan Travel Plan</h1>
             </nav>
         </div>
     )
